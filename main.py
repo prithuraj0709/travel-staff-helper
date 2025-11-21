442a74947e1d3086b7ad924ea7b5110456ac4a04
prithuraj0709 <prithuraj0709@gmail.com>
2025-11-21 12:30:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ except FileNotFoundError:
 # We get unique values from the 'City' column and sort them
 try:
     # REPLACE 'City' below with your exact CSV column name if different
-    city_list = sorted(df['City Code'].astype(str).unique())
+    city_list = sorted(df['City Code'].astype(str).unique()) 
     selected_city = st.selectbox("Step 1: Select City / City Code", city_list)
 
     # B. Filter data based on selected City
